f39406b318ecf3e8d727c670d5ec2cd87339b7ca
wu <wu@.com>
2025-04-10 15:39:43 +0800

    idank/bashlex/gpt-4o-0

--- setup.py
+++ setup.py
@@ -1,36 +1,35 @@
-#!/usr/bin/env python
-
-import os
 from setuptools import setup
 
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
 setup(
-    name="asserts",
-    version="0.11.0",
-    description="Stand-alone Assertions",
-    long_description=read("README.md"),
-    long_description_content_type="text/markdown",
-    author="Sebastian Rittau",
-    author_email="srittau@rittau.biz",
-    url="https://github.com/srittau/python-asserts",
-    packages=["asserts"],
-    package_data={"asserts": ["py.typed", "*.pyi"]},
-    python_requires=">=3.6",
-    license="MIT",
+    name='bashlex',
+    version='0.16',
+    url='https://github.com/idank/bashlex.git',
+    license='GPLv3+',
+    author='Idan Kamara',
+    author_email='idankk86@gmail.com',
+    description='Python parser for bash',
+    long_description='''bashlex is a Python port of the parser used internally by GNU bash.
+
+For the most part it's transliterated from C, the major differences are:
+
+1. it does not execute anything
+2. it is reentrant
+3. it generates a complete AST
+
+See https://github.com/idank/bashlex/blob/master/README.md for more info.''',
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: Software Development :: Quality Assurance",
-        "Topic :: Software Development :: Testing",
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: System Shells',
+        'Topic :: Text Processing',
     ],
+    python_requires=">=2.7, !=3.0, !=3.1, !=3.2, !=3.3, !=3.4",
+    install_requires=['enum34; python_version < "3.4"'],
+    packages=['bashlex'],
 )
