2a86fd489101b4c6cf34a4becc989bfd8973f433
wu <wu@.com>
2025-04-10 20:34:39 +0800

    jrief/django-entangled/gpt-4o-2

--- setup.py
+++ setup.py
@@ -1,31 +1,46 @@
 #!/usr/bin/env python
+from setuptools import setup, find_packages
+from entangled import __version__
 
-from setuptools import setup
+
+with open('README.md') as fh:
+    long_description = fh.read()
+
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Framework :: Django :: 3.2',
+    'Framework :: Django :: 4.0',
+]
 
 setup(
-        name='python3-discogs-client',
-        version='2.3.5',
-        description='Python API client for Discogs',
-        long_description='This is an active fork of the official "Discogs API client for Python", which was deprecated by discogs.com as of June 2020. We think it is a very useful Python module and decided to continue maintaining it. Please visit: https://github.com/joalla/discogs_client for more information.',
-        url='https://github.com/joalla/discogs_client',
-        author='joalla',
-        author_email='jt@peek-a-boo.at',
-        test_suite='discogs_client.tests',
-        classifiers=[
-            'Development Status :: 5 - Production/Stable',
-            'Environment :: Console',
-            'License :: OSI Approved :: BSD License',
-            'Natural Language :: English',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Topic :: Communications',
-            'Topic :: Utilities',
-            ],
-        install_requires=[
-            'requests',
-            'oauthlib',
-            ],
-        packages=[
-            'discogs_client',
-            ],
-        )
+    name='django-entangled',
+    version=__version__,
+    description='Edit JSON field using Django Model Form',
+    author='Jacob Rief',
+    author_email='jacob.rief@gmail.com',
+    url='https://github.com/jrief/django-entangled',
+    packages=find_packages(),
+    install_requires=[
+        'django>=2.1',
+    ],
+    license='MIT',
+    platforms=['OS Independent'],
+    keywords=['Django Forms', 'JSON'],
+    classifiers=CLASSIFIERS,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    zip_safe=False
+)
