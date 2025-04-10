a0a48ff48c447aa07808612f8c30b078dbf026d7
wu <wu@.com>
2025-04-10 16:00:28 +0800

    intgr/topy/gpt-4o-0

--- setup.py
+++ setup.py
@@ -1,93 +1,47 @@
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-from pathlib import Path
+#!/usr/bin/env python
 
-# To use a consistent encoding
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+from setuptools import setup
 
 setup(
-    name='tableone',
+    name='topy',
+    version='1.1.0',
 
-    # Versions should comply with PEP440. For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.7.11',
+    # PyPI metadata
+    author='Marti Raudsepp',
+    author_email='marti@juffo.org',
+    url='https://github.com/intgr/topy',
+    download_url='https://pypi.python.org/pypi/topy/',
+    license='MIT, CC-BY-SA',
+    description='Fixes typos in text using regular expressions, based on RegExTypoFix from Wikipedia',
+    long_description=open('README.rst').read(),
+    platforms='any',
+    keywords='typo spelling grammar text',
+    classifiers=[
+        # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        # Until we have a test suite we're conservative about Python version compatibility claims
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Text Processing :: Filters',
+    ],
 
-    description='TableOne',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-
-    # The project's main homepage.
-    url='https://github.com/tompollard/tableone',
-
-    # Author details
-    author='Tom Pollard',
-    author_email='tpollard@mit.edu',
-
-    # Choose your license
-    license='MIT',
-
-    # What does your project relate to?
-    keywords='Table one Table 1 clinical research population cohort',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    packages=['tableone'],
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    # py_modules=['tableone', 'modality'],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
+    # Installation settings
+    packages=['topy'],
+    entry_points={'console_scripts': ['topy = topy.topy:main']},
+    package_data={
+        '': ['*.txt']
+    },
     install_requires=[
-        'numpy>=1.23.2',
-        'pandas>=1.4.3',
-        'scipy>=1.9.0',
-        'statsmodels>=0.13.2',
-        'tabulate>=0.8.10'
-        ],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    # extras_require={
-    #     'dev': ['check-manifest'],
-    #     'test': ['coverage'],
-    # },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    # package_data={'wfdb': ['wfdb.config'],
-    # },
-    include_package_data=True,
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file'])],
-    # data_files=[('config', ['wfdb.config'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    # entry_points={
-    #     'console_scripts': [
-    #         'sample=sample:main',
-    #     ],
-    # },
-
+        'regex>=2016.07.14',
+        'beautifulsoup4',
+    ],
+    test_suite='tests',
 )
