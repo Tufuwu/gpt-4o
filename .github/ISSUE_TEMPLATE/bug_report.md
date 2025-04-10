2cd62b8396bd64bca97972efd866cadf5e950974
wu <wu@.com>
2025-04-10 15:46:47 +0800

    decalage2/olefile/gpt-4o-0

--- .github/ISSUE_TEMPLATE/bug_report.md
+++ .github/ISSUE_TEMPLATE/bug_report.md
@@ -1,35 +1,31 @@
 ---
 name: Bug report
-about: General bug report
-title: ''
-labels: ''
-assignees: ''
+about: Create a bug report to help us fix issues
 
 ---
-
-<!--
-Make sure to read
-https://python-fints.readthedocs.io/en/latest/trouble.html
-before you file a bug
--->
 
 **Describe the bug**
 A clear and concise description of what the bug is.
 
-**Bank I tested this with*
-Name of the bank:
-FinTS URL:
+**File or Malware sample to reproduce the bug**
+Please attach the file(s) that can be used to reproduce the bug.
+If this is malware, store it in a password protected zip archive, or provide a link where it can be downloaded (e.g. Hybrid Analysis, preferably not VirusTotal which requires paid access). If not possible, please provide a hash.
+
+**How To Reproduce the bug**
+Steps to reproduce the behavior, including the full command line or the options you used.
 
 **Expected behavior**
 A clear and concise description of what you expected to happen.
 
-**Code required to reproduce**
-```
-Your sample code
-```
+**Console output / Screenshots**
+If applicable, add screenshots to help explain your problem.
+Use the options "-c -d" when running olefile.py to add debugging information, if possible.
 
-**Log output / error message**
-<!-- Please make sure to remove all sensitive private information -->
+**Version information:**
+ - OS: Windows/Linux/Mac/Other
+ - OS version: x.xx - 32/64 bits
+ - Python version: 2.7/3.6 - 32/64 bits
+ - olefile version: 
 
 **Additional context**
 Add any other context about the problem here.
--- setup.py
+++ setup.py
@@ -1,46 +1,83 @@
-from codecs import open
-from os import path
+"""
+Installation script for olefile using setuptools
 
-from fints import version
-from setuptools import find_packages, setup
+To install this package, run:
+    python setup.py install
 
-here = path.abspath(path.dirname(__file__))
+"""
 
-try:
-    # Get the long description from the relevant file
-    with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = f.read()
-except:
-    long_description = ''
 
-setup(
-    name='fints',
-    version=version,
-    description='Pure-python FinTS 3.0 (formerly known as HBCI) implementation',
-    long_description=long_description,
-    url='https://github.com/raphaelm/python-fints',
-    author='Raphael Michel',
-    author_email='mail@raphaelmichel.de',
-    license='GNU Lesser General Public License v3 (LGPLv3)',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Other Audience',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-    ],
+# --- IMPORTS -----------------------------------------------------------------
 
-    keywords='hbci banking fints',
-    install_requires=[
-        'bleach',
-        'mt-940',
-        'requests',
-        'sepaxml~=2.1',
-    ],
+from setuptools import setup
 
-    packages=find_packages(include=['fints', 'fints.*']),
-)
+from olefile import __version__, __author__
+
+
+# --- METADATA ----------------------------------------------------------------
+
+name         = "olefile"
+version      = __version__
+desc         = "Python package to parse, read and write Microsoft OLE2 files (Structured Storage or Compound Document, Microsoft Office)"
+author       = __author__
+author_email = "nospam@decalage.info"
+url          = "https://www.decalage.info/python/olefileio"
+license      = "BSD"
+download_url = "https://github.com/decalage2/olefile/tarball/master"
+
+with open('README.md') as f:
+    long_desc = f.read()
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Information Technology",
+    "Intended Audience :: Science/Research",
+    "Intended Audience :: System Administrators",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Topic :: Software Development :: Libraries :: Python Modules"
+]
+
+# --- PACKAGES ----------------------------------------------------------------
+
+packages = [
+    "olefile",
+]
+
+
+# === MAIN ====================================================================
+
+def main():
+    setup(
+        name=name,
+        version=version,
+        description=desc,
+        long_description=long_desc,
+        long_description_content_type='text/markdown',
+        classifiers=classifiers,
+        author=author,
+        author_email=author_email,
+        url=url,
+        license=license,
+#        package_dir=package_dir,
+        packages=packages,
+        download_url=download_url,
+#        data_files=data_files,
+#        scripts=scripts,
+        python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    )
+
+
+if __name__ == "__main__":
+    main()
