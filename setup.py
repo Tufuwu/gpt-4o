327efc086792480ae6de204c70c1f5b79659b3ba
wu <wu@.com>
2025-04-10 15:47:29 +0800

    beaglecli/beagle/gpt-4o-0

--- setup.py
+++ setup.py
@@ -1,62 +1,28 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-import sys
-import re
-import codecs
+import setuptools
 
-from setuptools import setup, find_packages
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
 
-version = ''
-with open('pigar/version.py', 'r') as f:
-    version = re.search(
-        r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.M
-    ).group(1)
-
-if not version:
-    raise RuntimeError('Cannot find version information')
-
-with codecs.open('README-PYPI.rst', encoding='utf-8') as f:
-    long_description = f.read()
-
-with codecs.open('CHANGELOGS.rst', encoding='utf-8') as f:
-    change_logs = f.read()
-
-install_requires = [
-    'colorama>=0.3.9',
-    'requests>=2.20.0',
-]
-if sys.version_info < (3, 2):
-    install_requires.append('futures')
-
-setup(
-    name='pigar',
-    version=version,
-    description=(
-        'A fantastic tool to generate requirements for your'
-        ' Python project, and more than that.'
-    ),
-    long_description=long_description + '\n\n' + change_logs,
-    url='https://github.com/damnever/pigar',
-    author='damnever',
-    author_email='dxc.wolf@gmail.com',
-    license='The BSD 3-Clause License',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Topic :: Utilities',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
-    keywords='requirements tool',
-    packages=find_packages(),
-    install_requires=install_requires,
-    include_package_data=True,
-    entry_points={'console_scripts': [
-        'pigar=pigar.__main__:main',
-    ]},
-)
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True)
