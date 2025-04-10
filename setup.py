78bc06aaaf08b132f99d6af36dc3c91975d8532b
wu <wu@.com>
2025-04-10 15:52:14 +0800

    dakrauth/django-swingtime/gpt-4o-0

--- setup.py
+++ setup.py
@@ -1,23 +1,47 @@
-from setuptools import setup
+#!/usr/bin/env python
+import os, sys
+from setuptools import setup, find_packages
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit(0)
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+# Dynamically calculate the version based on swingtime.VERSION.
+version=__import__('swingtime').get_version()
 
 setup(
-    name="pytest-structlog",
-    version="0.5",
-    url="https://github.com/wimglenn/pytest-structlog",
-    description="Structured logging assertions",
-    long_description=open("README.rst").read(),
-    long_description_content_type="text/x-rst; charset=UTF-8",
-    author="Wim Glenn",
-    author_email="hey@wimglenn.com",
-    license="MIT",
-    install_requires=["pytest", "structlog"],
-    py_modules=["pytest_structlog"],
-    entry_points={"pytest11": ["pytest-structlog=pytest_structlog"]},
+    name='django-swingtime',
+    url='https://github.com/dakrauth/django-swingtime',
+    author='David A Krauth',
+    author_email='dakrauth@gmail.com',
+    description='A Django calendaring application.',
+    version=version,
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    platforms=['any'],
+    license='MIT License',
+    python_requires='>=3.6, <4',
+    install_requires=['Django>=2.2,<4.0', 'python-dateutil==2.8.0'],
     classifiers=[
-        "Framework :: Pytest",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Office/Business :: Scheduling',
     ],
-    options={"bdist_wheel": {"universal": "1"}},
+    packages=find_packages(),
+    package_data={'swingtime': ['locale/*/*/*.*',]},
+    zip_safe=False,
 )
