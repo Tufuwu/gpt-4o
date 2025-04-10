a0b4584218a05d6836e31a656b3def0c440f36cf
wu <wu@.com>
2025-04-10 19:07:19 +0800

    nmigen/nmigen-soc/gpt-4o-1

--- setup.py
+++ setup.py
@@ -1,47 +1,32 @@
 from setuptools import setup, find_packages
 
-version = "3.2.dev0"
 
-with open("README.rst", encoding="UTF-8") as file:
-    long_description = file.read() + "\n"
+def scm_version():
+    def local_scheme(version):
+        if version.tag and not version.distance:
+            return version.format_with("")
+        else:
+            return version.format_choice("+{node}", "+{node}.dirty")
+    return {
+        "relative_to": __file__,
+        "version_scheme": "guess-next-dev",
+        "local_scheme": local_scheme
+    }
 
-with open("HISTORY.txt", encoding="UTF-8") as file:
-    long_description += file.read()
 
 setup(
-    name="pyroma",
-    version=version,
-    description="Test your project's packaging friendliness",
-    long_description=long_description,
-    python_requires=">=3.6",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "License :: OSI Approved :: MIT License",
-        "Development Status :: 5 - Production/Stable",
-    ],
-    keywords=["pypi", "quality", "testing"],
-    author="Lennart Regebro",
-    author_email="regebro@gmail.com",
-    url="https://github.com/regebro/pyroma",
-    project_urls={"Source Code": "https://github.com/regebro/pyroma"},
-    license="MIT",
-    packages=find_packages(exclude=["ez_setup"]),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        "setuptools",
-        "docutils",
-        "pygments",
-    ],
-    entry_points={
-        "console_scripts": ["pyroma = pyroma:main"],
-        "zest.releaser.prereleaser.before": ["pyroma = pyroma:zester"],
+    name="nmigen-soc",
+    use_scm_version=scm_version(),
+    author="whitequark",
+    author_email="whitequark@whitequark.org",
+    description="System on Chip toolkit for nMigen",
+    #long_description="""TODO""",
+    license="BSD",
+    setup_requires=["wheel", "setuptools", "setuptools_scm"],
+    install_requires=["nmigen>=0.2,<0.5"],
+    packages=find_packages(),
+    project_urls={
+        "Source Code": "https://github.com/nmigen/nmigen-soc",
+        "Bug Tracker": "https://github.com/nmigen/nmigen-soc/issues",
     },
-    test_suite="pyroma",
 )
