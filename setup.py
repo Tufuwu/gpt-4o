ea69ca62215aef7b460302d8678f9a4089b10b88
wu <wu@.com>
2025-04-10 15:58:50 +0800

    eskerda/pybikes/gpt-4o-0

--- setup.py
+++ setup.py
@@ -1,49 +1,25 @@
-import os
-from distutils.core import setup
+# -*- coding: utf-8 -*-
+# Copyright (C) 2010-2012, eskerda <eskerda@gmail.com>
+# Distributed under the AGPL license, see LICENSE.txt
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+from setuptools import setup
 
 setup(
-    name='django-linkcheck',
-    version='1.8.1',
-    description="A Django app that will analyze and report on links in any "
-                "model that you register with it.",
-    long_description=read('README.rst'),
-    author='Andy Baker',
-    author_email='andy@andybak.net',
-    license='BSD',
-    url='https://github.com/DjangoAdminHackers/django-linkcheck',
-    packages=[
-        'linkcheck',
-        'linkcheck.management',
-        'linkcheck.management.commands',
-        'linkcheck.migrations',
-        'linkcheck.tests',
-        'linkcheck.tests.sampleapp',
-    ],
+    name="pybikes",
+    version="1.0",
+    author="Lluis Esquerda",
+    author_email="eskerda@gmail.com",
+    packages=["pybikes"],
     package_data={
-        'linkcheck': [
-            'templates/linkcheck/*.html',
-            'templates/linkcheck/*.xhtml',
-            'tests/media/*',
-        ]
+        'pybikes': ['data/*.json', 'kml/*.kml', 'kml/*.kml.gz'],
     },
-    install_requires=['requests'],
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Framework :: Django',
+    license="LICENSE.txt",
+    description="A python library for scrapping bike sharing data",
+    long_description=open('README.md').read(),
+    install_requires=[
+        'requests>=2.20.0',
+        'lxml',
+        'cssselect>=0.9',
+        'shapely>=1.5.13',
     ],
 )
