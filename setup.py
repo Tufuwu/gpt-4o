4ae129e4dec6d508b44fb719a3b518f73f4f780c
wu <wu@.com>
2025-04-10 15:42:50 +0800

    uos/rospy_message_converter/gpt-4o-0

--- setup.py
+++ setup.py
@@ -1,23 +1,11 @@
-from setuptools import setup
+# ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
-setup(name='cycler',
-      version='0.10.0',
-      author='Thomas A Caswell',
-      author_email='matplotlib-users@python.org',
-      py_modules=['cycler'],
-      description='Composable style cycles',
-      url='https://github.com/matplotlib/cycler',
-      platforms='Cross platform (Linux, Mac OSX, Windows)',
-      license="BSD",
-      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
-      classifiers=['License :: OSI Approved :: BSD License',
-                   'Development Status :: 4 - Beta',
-                   'Programming Language :: Python :: 2',
-                   'Programming Language :: Python :: 2.7',
-                   'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 3.4',
-                   'Programming Language :: Python :: 3.5',
-                   'Programming Language :: Python :: 3.6',
-                   ],
-      keywords='cycle kwargs',
-      )
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['rospy_message_converter'],
+    package_dir={'': 'src'},
+)
+
+setup(**d)
